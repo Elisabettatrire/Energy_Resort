2033fa42781983c127f1ed661e338b242908da69
Rux91 <rux91@LAPTOP-L3B06VLG.homenet.telecomitalia.it>
2020-08-20 11:14:20 +0200

    secondo push

---
+++ Energy_Resort/src/agents/BaseAgent.java
@@ -0,0 +1,131 @@
+package agents;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import jade.core.AID;
+import jade.core.Agent;
+import jade.domain.DFService;
+import jade.domain.FIPAException;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.lang.acl.ACLMessage;
+
+@SuppressWarnings("serial")
+public class BaseAgent extends Agent {
+	
+	protected void registerDfAgent(String platform, String type, String idType)
+	{
+		DFAgentDescription ad = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setName("Platform-"+platform);
+		sd.setType(type);
+		
+		ServiceDescription sd1 = new ServiceDescription();
+		sd1.setName("Platform-"+platform);
+		sd1.setType(idType);
+		
+		ad.addServices(sd1);
+		ad.addServices(sd);
+		
+		try{
+			DFService.register(this, ad);
+		}catch(FIPAException e){
+			e.printStackTrace();
+		}
+	}
+	
+	protected void registerDfAgent(String platform, String type)
+	{
+		DFAgentDescription ad = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setName("Platform-"+platform);
+		sd.setType(type);
+		ad.addServices(sd);
+		
+		try{
+			DFService.register(this, ad);
+		}catch(FIPAException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public Boolean sendMessageToAgentsByServiceType (Agent myAgent, String serviceType, 
+			String conversationId, Serializable messageData)
+	{
+		try {
+			DFAgentDescription[] agents = getAgentsbyServiceType(myAgent, serviceType);
+			for(int i=0; i<agents.length; i++)
+			{
+				ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+				message.setContentObject(messageData);
+				message.addReceiver(agents[i].getName()); 
+				message.setConversationId(conversationId);
+				myAgent.send(message);
+			}
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public Boolean sendMessageToAgentsByServiceType (Agent myAgent, AID receiver, 
+			String conversationId, Serializable messageData)
+	{
+		try {
+			ACLMessage message = new ACLMessage(ACLMessage.INFORM);
+			message.setContentObject(messageData);
+			message.addReceiver(receiver); 
+			message.setConversationId(conversationId);
+			myAgent.send(message);
+			return true;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	public DFAgentDescription[] getAgentsbyServiceType (Agent myAgent, String serviceType)
+	{
+		DFAgentDescription ad = new DFAgentDescription();
+		ServiceDescription sd = new ServiceDescription();
+		sd.setType(serviceType);
+		ad.addServices(sd);
+		DFAgentDescription[] ca = null;
+		try {
+			ca = DFService.search(myAgent, ad);
+		} catch (FIPAException e) {
+			e.printStackTrace();
+		}
+		return ca;
+	}
+	
+	public String getShortName (String idAgent)
+	{
+		String[] names = idAgent.split("@");
+		return names[0];
+	}
+	
+	
+	protected void takeDown()
+	{
+		if (this.getDefaultDF() == null)
+		{
+			try {
+				DFService.deregister(this);
+			} catch (FIPAException e) {
+				e.printStackTrace();
+			}
+		}
+		else
+		{
+			try {
+				DFService.deregister(this, this.getDefaultDF());
+			} catch (FIPAException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+}
---
+++ Energy_Resort/src/utils/PlatformCreator.java
@@ -0,0 +1,64 @@
+package utils;
+
+import jade.core.Profile;
+import jade.core.ProfileImpl;
+import jade.core.Runtime;
+import jade.wrapper.AgentController;
+import jade.wrapper.ContainerController;
+import jade.wrapper.StaleProxyException;
+
+/**
+ * Applicazione esterna che gestisce il lancio di una piattaforma ad agenti,
+ * attraverso la creazione di un main container e la creazione di un'istanza di Agent0
+ *
+ */
+public class PlatformCreator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/**
+		 * Recupero dell'istanza della classe Runtime
+		 */
+		Runtime rt = Runtime.instance();
+		
+		/**
+		 * Creazione di un main container:
+		 * - il Profile contiene eventuali parametri di configurazione
+		 * - createMainContainer(p) crea il main container della piattaforma
+		 */
+		Profile p = new ProfileImpl();
+		p.setParameter("gui", "true");
+		ContainerController cc = rt.createMainContainer(p);
+		
+		try {
+			AgentController tso = cc.createNewAgent("Tsololol", "agents.TsoAgent", null);
+			/*	AgentController grid = cc.createNewAgent("GridAgent", "agents.GridAgent", null);
+			AgentController control = cc.createNewAgent("ControlAgent", "agents.GridAgent", null);
+			AgentController batteryAggregator = cc.createNewAgent("BatteryAggregator", "agents.GridAgent", null);
+			AgentController derAggregator = cc.createNewAgent("DerAggregator", "agents.GridAgent", null);
+			AgentController loadAggregator = cc.createNewAgent("LoadAggregator", "agents.GridAgent", null);
+			AgentController battery = cc.createNewAgent("Battery", "agents.GridAgent", null);
+			AgentController photo = cc.createNewAgent("Photo", "agents.GridAgent", null);
+			AgentController hydro = cc.createNewAgent("Hydro", "agents.GridAgent", null);
+			
+			tso.start();
+			grid.start();
+			control.start();
+			batteryAggregator.start();
+			derAggregator.start();
+			loadAggregator.start();
+			battery.start();
+			photo.start();
+			hydro.start();*/
+		} catch (StaleProxyException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
