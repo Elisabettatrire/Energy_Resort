5fea3bfe831d047997164f9bc0984a6fce8b7fd2
elisabettatrire <elisabettatrire@gmail.com>
2020-09-18 12:09:05 +0200

    creata SolarData

--- Energy_Resort/src/data/SolarData.java
+++ Energy_Resort/src/data/SolarData.java
@@ -1,5 +1,66 @@
 package data;
 
 public class SolarData {
+	
+	private double solarPrice;
+	private double solarKw;
+	private int solarForecast; //le previsioni del sole
+	private int dayHour;
+	private int weekDay;
+	
+	public SolarData(double solarPrice, double solarKw, int solarForecast, int dayHour, int weekDay) {
+
+		this.solarPrice = solarPrice;
+		this.solarKw = solarKw;
+		this.solarForecast = solarForecast;
+		this.dayHour= dayHour;
+		this.weekDay= weekDay;
+	}
+	
+	public SolarData() {
+		
+	}
+	
+	public double getSolarPrice() {
+		return solarPrice;
+	}
+
+	public void setSolarPrice(double solarPrice) {
+		this.solarPrice = solarPrice;
+	}
+
+	public double getSolarKw() {
+		return solarKw;
+	}
+
+	public void setSolarKw(double solarKw) {
+		this.solarKw = solarKw;
+	}
+
+	public int getSolarForecast() {
+		return solarForecast;
+	}
+
+	public void setSolarForecast(int solarForecast) {
+		this.solarForecast = solarForecast;
+	}
+
+	public int getDayHour() {
+		return dayHour;
+	}
+
+	public void setDayHour(int dayHour) {
+		this.dayHour = dayHour;
+	}
+
+	public int getWeekDay() {
+		return weekDay;
+	}
+
+	public void setWeekDay(int weekDay) {
+		this.weekDay = weekDay;
+	}
+	
+	
 
 }
