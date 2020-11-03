3cd26f4fbb3e9a74431dcf0c05d152de453f593d
Rux91 <44968803+Rux91@users.noreply.github.com>
2020-11-03 16:32:13 +0100

    Update DbConnection.java

--- Energy_Resort/src/database/DbConnection.java
+++ Energy_Resort/src/database/DbConnection.java
@@ -9,10 +9,10 @@ import java.sql.Statement;
 public class DbConnection {
 
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:8889/EnergyResort_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/EnergyResort_db?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 
 	static final String USER = "root";
-	static final String PASS = "root";
+	static final String PASS = "";
 
 	public Connection conn = null;
 	public Statement stmt = null;
